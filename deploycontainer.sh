18d5ba3424b4f50a9ffeb5e5acb0e45d998f88d3
Sridhar Madhugiri <sridmad@microsoft.com>
2018-01-18 14:44:59 -0800

    travis CI integration files

---
+++ deploycontainer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+TAG=$1
+BRANCH=$2
+DOCKER_USERNAME=$3
+DOCKER_PASSWORD=$4
+
+if [ $BRANCH = "master" ]; then
+    BRANCH=microsoft
+fi
+
+echo TAG=$TAG
+echo BRANCH=$BRANCH
+echo DOCKER_USERNAME=$DOCKER_USERNAME
+echo DOCKER_PASSWORD=$DOCKER_PASSWORD
+
+echo dotnet publish -c Release sfintegration
+dotnet publish -c Release sfintegration
+
+echo docker build -t reverseproxyimage ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+docker build -t reverseproxyimage ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+
+# Build the Docker images
+echo docker build -t $BRANCH/reverseproxyimage:$TAG ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+docker build -t $BRANCH/reverseproxyimage:$TAG ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+echo docker tag $BRANCH/reverseproxyimage:$TAG $BRANCH/reverseproxyimage:latest
+docker tag $BRANCH/reverseproxyimage:$TAG $BRANCH/reverseproxyimage:latest
+
+# Login to Docker Hub and upload images
+echo docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+echo docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+echo docker push $BRANCH/reverseproxyimage:$TAG
+echo docker push $BRANCH/reverseproxyimage:$TAG
+echo docker push $BRANCH/reverseproxyimage:latest
+echo docker push $BRANCH/reverseproxyimage:latest
