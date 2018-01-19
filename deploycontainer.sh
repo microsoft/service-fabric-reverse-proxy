b1c0252f9c402be08dd388bdade03e0d417a0a3a
Sridhar Madhugiri <sridmad@microsoft.com>
2018-01-19 15:10:13 -0800

    Update version and the tags

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=0.2.0
+TAG=0.4.0
 BRANCH=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
@@ -9,25 +9,29 @@ if [ $BRANCH = "master" ]; then
     BRANCH=microsoft
 fi
 
-echo -e \e[92TAG=$TAG
-echo -e \e[92BRANCH=$BRANCH
+echo TAG=$TAG
+echo BRANCH=$BRANCH
 
 config=Release
 
-echo -e \e[92dotnet build -c $config sfintegration
+echo dotnet build -c $config sfintegration
 dotnet build -c $config sfintegration
-echo -e \e[92dotnet publish -c $config sfintegration
+echo dotnet publish -c $config sfintegration
 dotnet publish -c $config sfintegration
 
 # Build the Docker images
-echo -e \e[92docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.0/publish/.
+echo docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.0/publish/.
 docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.0/publish/.
-echo -e \e[92docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
+echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
+docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
+echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
 docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
 
 # Login to Docker Hub and upload images
 echo $DOCKER_PASSWORD | docker login -u="$DOCKER_USERNAME" --password-stdin
-echo -e \e[92docker push $BRANCH/service-fabric-reverse-proxy:$TAG
+echo docker push $BRANCH/service-fabric-reverse-proxy:$TAG
 docker push $BRANCH/service-fabric-reverse-proxy:$TAG
-echo -e \e[92docker push $BRANCH/service-fabric-reverse-proxy:latest
+echo docker push $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
+docker push $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
+echo docker push $BRANCH/service-fabric-reverse-proxy:latest
 docker push $BRANCH/service-fabric-reverse-proxy:latest
