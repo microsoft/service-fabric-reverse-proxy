f151c9ee0790b75be79da2dfb67080ccc6be8928
Sridhar Madhugiri <sridmad@microsoft.com>
2018-01-18 17:15:14 -0800

    Add docker as a required service and hard code tag for testing builds

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-TAG=$1
-BRANCH=$2
-DOCKER_USERNAME=$3
-DOCKER_PASSWORD=$4
+TAG=0.1.0
+BRANCH=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
 
 if [ $BRANCH = "master" ]; then
     BRANCH=microsoft
@@ -20,19 +20,19 @@ dotnet build -c Release sfintegration
 echo dotnet publish -c Release sfintegration
 dotnet publish -c Release sfintegration
 
-echo docker build -t reverseproxyimage ./sfintegration/bin/Release/netcoreapp2.0/publish/.
-docker build -t reverseproxyimage ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+echo docker build -t service-fabric-reverse-proxy ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+docker build -t service-fabric-reverse-proxy ./sfintegration/bin/Release/netcoreapp2.0/publish/.
 
 # Build the Docker images
-echo docker build -t $BRANCH/reverseproxyimage:$TAG ./sfintegration/bin/Release/netcoreapp2.0/publish/.
-docker build -t $BRANCH/reverseproxyimage:$TAG ./sfintegration/bin/Release/netcoreapp2.0/publish/.
-echo docker tag $BRANCH/reverseproxyimage:$TAG $BRANCH/reverseproxyimage:latest
-docker tag $BRANCH/reverseproxyimage:$TAG $BRANCH/reverseproxyimage:latest
+echo docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/Release/netcoreapp2.0/publish/.
+echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
+docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
 
 # Login to Docker Hub and upload images
 echo docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 echo docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-echo docker push $BRANCH/reverseproxyimage:$TAG
-echo docker push $BRANCH/reverseproxyimage:$TAG
-echo docker push $BRANCH/reverseproxyimage:latest
-echo docker push $BRANCH/reverseproxyimage:latest
+echo docker push $BRANCH/service-fabric-reverse-proxy:$TAG
+echo docker push $BRANCH/service-fabric-reverse-proxy:$TAG
+echo docker push $BRANCH/service-fabric-reverse-proxy:latest
+echo docker push $BRANCH/service-fabric-reverse-proxy:latest
