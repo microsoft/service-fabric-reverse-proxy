bdd9a84e0e6c4860795ca74126d752a34a36119a
Sridhar Madhugiri <sridmad@microsoft.com>
2018-01-19 01:27:33 -0800

    cleanup scripts

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 TAG=0.2.0
 BRANCH=$1
@@ -10,25 +9,25 @@ if [ $BRANCH = "master" ]; then
     BRANCH=microsoft
 fi
 
-echo TAG=$TAG
-echo BRANCH=$BRANCH
+echo -e \e[92TAG=$TAG
+echo -e \e[92BRANCH=$BRANCH
 
 config=Release
 
-echo dotnet build -c $config sfintegration
+echo -e \e[92dotnet build -c $config sfintegration
 dotnet build -c $config sfintegration
-echo dotnet publish -c $config sfintegration
+echo -e \e[92dotnet publish -c $config sfintegration
 dotnet publish -c $config sfintegration
 
 # Build the Docker images
-echo docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.0/publish/.
+echo -e \e[92docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.0/publish/.
 docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.0/publish/.
-echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
+echo -e \e[92docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
 docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
 
 # Login to Docker Hub and upload images
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-#echo docker push $BRANCH/service-fabric-reverse-proxy:$TAG
+echo $DOCKER_PASSWORD | docker login -u="$DOCKER_USERNAME" --password-stdin
+echo -e \e[92docker push $BRANCH/service-fabric-reverse-proxy:$TAG
 docker push $BRANCH/service-fabric-reverse-proxy:$TAG
-#echo docker push $BRANCH/service-fabric-reverse-proxy:latest
+echo -e \e[92docker push $BRANCH/service-fabric-reverse-proxy:latest
 docker push $BRANCH/service-fabric-reverse-proxy:latest
