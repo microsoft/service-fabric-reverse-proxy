00990dab1741130f0863bb6133811680f293ecec
Sridhar Madhugiri <sridmad@microsoft.com>
2018-08-04 11:55:36 -0700

    log to stdout

--- sfintegration/startup.sh
+++ sfintegration/startup.sh
@@ -18,7 +18,7 @@ else
     then
         if [ -z "${ReverseProxyCertThumbprint}" ]
         then
-            echo $(timestamp), startup.sh, Invalid Reverse Proxy Thumbprint > ${reverse_proxy_log_path}.log
+            echo $(timestamp), startup.sh, Invalid Reverse Proxy Thumbprint | tee ${reverse_proxy_log_path}.log
             exit 1
         fi
         sed -e s/ReverseProxyCertThumbprint/$(echo ${ReverseProxyCertThumbprint} | sed 's/[&/\]/\\&/g')/g \
@@ -29,21 +29,21 @@ else
     fi
 fi
 
-if [ ${Fabric_NodeName} == "" ]
+if [ "${Fabric_NodeName}" == "" ]
 then
     Fabric_NodeName="standalone"
 fi
 
-echo $(timestamp), startup.sh, Begin validate Envoy cofigfile, $config_file >> ${reverse_proxy_log_path}.log
-echo /usr/local/bin/envoy -c ${config_file} --service-cluster ReverseProxy --service-node ${Fabric_NodeName} --mode validate >> ${reverse_proxy_log_path}.log 2>&1
-/usr/local/bin/envoy -c ${config_file} --service-cluster ReverseProxy --service-node ${Fabric_NodeName} --mode validate >> "${reverse_proxy_log_path}.log" 2>&1
+echo $(timestamp), startup.sh, Begin validate Envoy cofigfile, $config_file | tee ${reverse_proxy_log_path}.log
+echo /usr/local/bin/envoy -c ${config_file} --service-cluster ReverseProxy --service-node ${Fabric_NodeName} --mode validate | tee ${reverse_proxy_log_path}.log 2>&1
+/usr/local/bin/envoy -c ${config_file} --service-cluster ReverseProxy --service-node ${Fabric_NodeName} --mode validate | tee "${reverse_proxy_log_path}.log" 2>&1
 retval=$?
 if [ $retval -eq 1 ]
 then
-    echo $(timestamp), startup.sh, Failed validate Envoy cofigfile, $config_file >> "${reverse_proxy_log_path}.log" 2>&1
+    echo $(timestamp), startup.sh, Failed validate Envoy cofigfile, $config_file | tee "${reverse_proxy_log_path}.log" 2>&1
     exit 1
 fi
-echo $(timestamp), startup.sh, Succeeded validate Envoy cofigfile, $config_file >> "${reverse_proxy_log_path}.log" 2>&1
+echo $(timestamp), startup.sh, Succeeded validate Envoy cofigfile, $config_file | tee "${reverse_proxy_log_path}.log" 2>&1
 
-/usr/local/bin/envoy -l info -c ${config_file} --service-cluster ReverseProxy --service-node ${Fabric_NodeName} >> "${reverse_proxy_log_path}.envoy.log" 2>&1 &
-LD_LIBRARY_PATH=/opt/microsoft/servicefabric/bin/Fabric/Fabric.Code:. FabricPackageFileName= dotnet sfintegration.dll >> "${reverse_proxy_log_path}.sfintegration.log" 2>&1
+/usr/local/bin/envoy -l info -c ${config_file} --service-cluster ReverseProxy --service-node ${Fabric_NodeName} | tee "${reverse_proxy_log_path}.envoy.log" 2>&1 &
+LD_LIBRARY_PATH=/opt/microsoft/servicefabric/bin/Fabric/Fabric.Code:. FabricPackageFileName= dotnet sfintegration.dll | tee "${reverse_proxy_log_path}.sfintegration.log" 2>&1
