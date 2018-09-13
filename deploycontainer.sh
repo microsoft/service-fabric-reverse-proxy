1546299940f1a56eb5d0ae51a92f457e5108816d
Sridhar Madhugiri <sridmad@microsoft.com>
2018-09-13 13:06:42 -0700

    label the  container with git commit

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -2,15 +2,21 @@
 
 TAG=0.20.0
 BRANCH=$1
-DOCKER_USERNAME=$2
-DOCKER_PASSWORD=$3
+GIT_COMMIT=$2
+DOCKER_USERNAME=$3
+DOCKER_PASSWORD=$4
 
 if [ $BRANCH = "master" ]; then
     BRANCH=microsoft
 fi
 
+if [ -z $GIT_COMMIT ]; then
+    GIT_COMMIT="Unknown"
+fi
+
 echo TAG=$TAG
 echo BRANCH=$BRANCH
+echo GIT_COMMIT=$GIT_COMMIT
 
 config=Release
 
@@ -23,8 +29,8 @@ dotnet publish -c $config sfintegration
 docker pull $BRANCH/service-fabric-reverse-proxy:latest
 
 # Build the Docker images
-echo docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
-docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
+echo docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG --label GIT_COMMIT=$GIT_COMMIT ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
+docker build -t $BRANCH/service-fabric-reverse-proxy:$TAG --label GIT_COMMIT=$GIT_COMMIT ./sfintegration/bin/$config/netcoreapp2.1/ubuntu.16.04-x64/publish/.
 echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
 docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:xenial-$TAG
 echo docker tag $BRANCH/service-fabric-reverse-proxy:$TAG $BRANCH/service-fabric-reverse-proxy:latest
